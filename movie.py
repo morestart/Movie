70f53106a175894518545b3940611b1390567b5f
morestart <morestarforyou@gmail.com>
2018-05-17 08:35:00 +0800

    移除threading模块,改用APScheduler非阻塞调用任务

--- movie.py
+++ movie.py
@@ -69,18 +69,34 @@ def async_setup(hass, config=None):
 
     hass.components.persistent_notification.async_create(
         con, "最近上映的电影")
-
+    start_job()
     return True
 
 
 def start_job():
-    from apscheduler.schedulers.blocking import BlockingScheduler
-    scheduler = BlockingScheduler()
-    scheduler.add_job(restart_ha, 'cron', day_of_week='0', hour=2, minute=00)
+    """
+    minute： 表示分钟，可以是从0到59之间的任何整数。
+
+    hour：表示小时，可以是从0到23之间的任何整数。
+
+    day：表示日期，可以是从1到31之间的任何整数。
+
+    month：表示月份，可以是从1到12之间的任何整数。
+
+    week：表示星期几，可以是从0到7之间的任何整数，这里的0或7代表星期日。
+
+    星号（*）：代表所有可能的值，例如month字段如果是星号，则表示在满足其它字段的制约条件后每月都执行该命令操作。
+
+    逗号（,）：可以用逗号隔开的值指定一个列表范围，例如，“1,2,5,7,8,9”
+
+    中杠（-）：可以用整数之间的中杠表示一个整数范围，例如“2-6”表示“2,3,4,5,6”
+
+    正斜线（/）：可以用正斜线指定时间的间隔频率，例如“0-23/2”表示每两小时执行一次。同时正斜线可以和星号一起使用，例如*/10，
+    如果用在minute字段，表示每十分钟执行一次。
+
+    :return:
+    """
+    from apscheduler.schedulers.background import BackgroundScheduler
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(restart_ha, 'cron', day_of_week='0', hour=8, minute=00)
     scheduler.start()
-
-
-import threading
-
-t = threading.Thread(target=start_job)
-t.start()
